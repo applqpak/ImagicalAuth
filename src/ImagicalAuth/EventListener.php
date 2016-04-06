83e4808668e59685897501708e9d80a8003d4ee4
applqpak <applqpak@gmail.com>
2016-04-06 19:32:31 +1000

    Create EventListener.php

---
+++ src/ImagicalAuth/EventListener.php
@@ -0,0 +1,15 @@
+<?php
+
+  namespace ImagicalAuth;
+
+  use pocketmine\plugin\PluginBase;
+  use pocketmine\event\Listener;
+  use pocketmine\utils\TextFormat as TF;
+  use pocketmine\event\player\PlayerJoinEvent;
+
+  class EventListener extends PluginBase implements Listener
+  {
+
+  }
+
+?>
