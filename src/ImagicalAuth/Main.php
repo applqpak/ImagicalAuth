b845a08aeb4d1152166518998e256bc7e707bd61
applqpak <applqpak@gmail.com>
2016-04-06 19:34:13 +1000

    Update Main.php

--- src/ImagicalAuth/Main.php
+++ src/ImagicalAuth/Main.php
@@ -3,11 +3,26 @@
   namespace ImagicalAuth;
 
   use pocketmine\plugin\PluginBase;
+  use pocketmine\event\Listener;
   use pocketmine\utils\Config;
   use pocketmine\utils\TextFormat as TF;
 
   class Main extends PluginBase
   {
+
+    public function dataPath()
+    {
+
+      return $this->getDataFolder();
+
+    }
+
+    public function server()
+    {
+
+      return $this->getServer();
+
+    }
 
   }
 
