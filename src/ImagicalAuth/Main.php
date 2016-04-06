40585e872e90f9509a712d1a9e59a4d317e86622
applqpak <applqpak@gmail.com>
2016-04-06 19:30:38 +1000

    Create Main.php

---
+++ src/ImagicalAuth/Main.php
@@ -0,0 +1,14 @@
+<?php
+
+  namespace ImagicalAuth;
+
+  use pocketmine\plugin\PluginBase;
+  use pocketmine\utils\Config;
+  use pocketmine\utils\TextFormat as TF;
+
+  class Main extends PluginBase
+  {
+
+  }
+
+?>
